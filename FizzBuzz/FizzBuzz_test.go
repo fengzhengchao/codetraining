462739bf86ada1459f8383b0c5a2d19642897317
georgefeng <georgefeng@tencent.com>
2019-06-13 22:28:23 +0800

    modify test

--- FizzBuzz/FizzBuzz_test.go
+++ FizzBuzz/FizzBuzz_test.go
@@ -1,50 +1,30 @@
 package FizzBuzz
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func Test_sayFizz(t *testing.T) {
 	TwoSpecialNumber(3,5)
-	slogan0 := saySlogan(6)
-	slogan1 := saySlogan(13)
-	if slogan0 == "Fizz" &&  slogan1 == "Fizz" {
-		t.Log("第一个测试通过了")
-	}else {
-		t.Error("第一个测试没通过")
-	}
+	assert.Equal(t, saySlogan(3), "Fizz")
 }
 
 func Test_sayBuzz(t *testing.T) {
 	TwoSpecialNumber(3,5)
-	slogan0 := saySlogan(52)
-	slogan1 := saySlogan(10)
-	if  slogan0 == "Buzz" && slogan1 == "Buzz" {
-		t.Log("第二个测试通过了")
-	}else {
-		t.Error("第二个测试没通过")
-	}
+	assert.Equal(t, saySlogan(52), "Buzz")
+	assert.Equal(t, saySlogan(10), "Buzz")
 }
 
 func Test_sayFizzBuzz(t *testing.T) {
 	TwoSpecialNumber(3,5)
-	slogan0 := saySlogan(51)
-	slogan1 := saySlogan(15)
-	if slogan0 == "FizzBuzz" && slogan1 == "FizzBuzz" {
-		t.Log("第三个测试通过了")
-	}else {
-		t.Error("第三个测试没通过")
-	}
+	assert.Equal(t, saySlogan(51), "FizzBuzz")
+	assert.Equal(t, saySlogan(15), "FizzBuzz")
 }
 
 func Test_sayNum(t *testing.T) {
 	TwoSpecialNumber(3,5)
-	slogan := saySlogan(7)
-	if  slogan == "7" {
-		t.Log("第四个测试通过了")
-	}else {
-		t.Error("第四个测试没通过")
-	}
+	assert.Equal(t, saySlogan(7), "7")
 }
 
 func Test_startGame(t *testing.T) {
